df550fd15047d6e8feff9cf32cdeadec2298db51
Kirin <antony.spruce@mail.ru>
2023-08-24 13:16:36 +0300

    dz_5

--- advertisement/advertisement/urls.py
+++ advertisement/advertisement/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('proba/', include('app_advertisement.urls')),
+    path('', include('app_advertisement.urls')),
     path('lesson_4', include('app_lesson_4.urls')),
 ]
