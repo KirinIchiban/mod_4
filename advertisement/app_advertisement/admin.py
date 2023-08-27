48a81867bdc2ab5fd7328b67dbb5165024066439
Kirin <antony.spruce@mail.ru>
2023-08-27 11:26:03 +0300

    Добавление функции get_image

--- advertisement/app_advertisement/admin.py
+++ advertisement/app_advertisement/admin.py
@@ -4,7 +4,8 @@ from .models import Advertisement
 # Register your models here.
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 
+                    'image', 'updated_date', 'auction', 'get_image']
     list_filter = ['auction', 'created_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     search_fields = ['title']
@@ -16,6 +17,8 @@ class AdvertisementAdmin(admin.ModelAdmin):
     @admin.action(description='Добавить возможность торга')
     def make_auction_as_true(self, request, queryset):
         queryset.update(auction=True)
+
+
 
 
 admin.site.register(Advertisement, AdvertisementAdmin)
